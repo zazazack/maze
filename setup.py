4a5161d03bd9ca55919cf1d56ebe72ce7e2ff415
Zachary Wilson <wilsonze@gmail.com>
2018-03-03 20:03:02 -0600

    Ran setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    dict(
-        name='maze'
-    )
+    name='maze',
+    version='0.0.0',
+    packages=find_packages()
 )
